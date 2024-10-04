48f6c7b6442c426e23807f2ab3deec9df5f51979
francis-d2020 <francis_d95@hotmail.>
2024-10-05 00:43:49 +0300

    done with the mvc ticketing project

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -86,6 +86,7 @@ public class ProjectController {
             model.addAttribute("managers", userService.findManagers());
             model.addAttribute("projects", projectService.findAll());
 
+
             return "/project/update";
 
         }
